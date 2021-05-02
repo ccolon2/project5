b46370bb06e5ebb96e5a88594dabbe821ed349da
ccolon2 <65677366+ccolon2@users.noreply.github.com>
2021-05-01 23:36:17 -0700

    Update fs.c

--- fs.c
+++ fs.c
@@ -36,7 +36,39 @@ union fs_block {
 
 int fs_format()
 {
-	return 0;
+	{
+	//Read in super block
+	union fs_block block;
+	disk_read(0, block.data);
+
+	//Check if FS already mounted
+	if (mounted){
+		printf("FS is already mounted, format failed\n");
+	 	return 0;
+	}
+
+	//Create superblock, prepare for mount
+	int ninodeblocks = ceil(.1 * (double)disk_size());
+	block.super.magic = FS_MAGIC;
+	block.super.nblocks = disk_size();
+	block.super.ninodeblocks = ninodeblocks;
+	block.super.ninodes = INODES_PER_BLOCK * ninodeblocks;
+
+	// Write changes to disk
+	disk_write(0, block.data);
+
+	//Clear the inode table
+	union fs_block iblock;
+	for(int i=1; i<=block.super.ninodeblocks; i++){
+		// Read in inode block
+		disk_read(i, iblock.data);
+		for(int j=0; j<INODES_PER_BLOCK; j++){
+			iblock.inode[j].isvalid = 0;
+		}
+		disk_write(i, iblock.data);
+	}
+
+	return 1;
 }
 
 void fs_debug()
