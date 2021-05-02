99b33b97d93c567127781d5b33b5bee900464601
ccolon2 <65677366+ccolon2@users.noreply.github.com>
2021-05-01 23:38:27 -0700

    Update fs.c

--- fs.c
+++ fs.c
@@ -73,14 +73,55 @@ int fs_format()
 
 void fs_debug()
 {
+	
 	union fs_block block;
+	union fs_block indirect_block;
 
+	// Read in super block
 	disk_read(0,block.data);
+	int valid_super_block = check_magic(block.super.magic);
 
 	printf("superblock:\n");
+	if(valid_super_block)
+		printf("    magic number is valid\n");
+	else{
+		printf("    magic number is not valid\n");
+		return;
+	}
 	printf("    %d blocks\n",block.super.nblocks);
 	printf("    %d inode blocks\n",block.super.ninodeblocks);
 	printf("    %d inodes\n",block.super.ninodes);
+
+	// Go through inode blocks
+	for(int i=1; i<=block.super.ninodeblocks; i++){ //added equal
+		// Read in inode block
+		disk_read(i, block.data);
+
+		// Traverse inodes
+		for(int j = 0; j<INODES_PER_BLOCK; j++) {
+			// Check if inode is valid
+			if(block.inode[j].isvalid) {
+				int inumber = get_inum(i, j);
+				printf("inode %d:\n", inumber);
+				printf("    size: %d bytes\n", block.inode[j].size);
+
+				// Traverse direct pointers
+				if(block.inode[j].size > 0){
+					printf("    direct blocks: ");
+					print_valid_blocks(block.inode[j].direct, POINTERS_PER_INODE);
+				}
+
+				// Traverse indirect pointers
+				if(block.inode[j].indirect != 0){
+					printf("    indirect block: %d\n", block.inode[j].indirect);
+					printf("    indirect data blocks: ");
+					disk_read(block.inode[j].indirect, indirect_block.data);
+					print_valid_blocks(indirect_block.pointers, POINTERS_PER_BLOCK);
+				}
+			}
+		}
+
+	}
 }
 
 int fs_mount()
