ef8656c34cc84a9212f60443c96a82b275ff146e
root <ccolon2@nd.edu>
2021-05-02 17:46:15 +0000

    format

--- fs.c
+++ fs.c
@@ -7,12 +7,16 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
+#include <math.h>
+#include <stdbool.h>
+
 
 #define FS_MAGIC           0xf0f03410
 #define INODES_PER_BLOCK   128
 #define POINTERS_PER_INODE 5
 #define POINTERS_PER_BLOCK 1024
-
+int *bitmap;
+int mounted = 0;
 struct fs_superblock {
 	int magic;
 	int nblocks;
@@ -34,23 +38,8 @@ union fs_block {
 	char data[DISK_BLOCK_SIZE];
 };
 
-int check_magic(int magic){
-	return (magic == FS_MAGIC);
-}
-
-void print_valid_blocks(int array[], int size){
-	for(int i=0; i< size; i++){
-		if(array[i] == 0){ 
-			continue;
-		}
-		printf("%d ",array[i]);
-	}
-	printf("\n");
-}
-
 int fs_format()
 {
-	{
 	//Read in super block
 	union fs_block block;
 	disk_read(0, block.data);
@@ -87,55 +76,14 @@ int fs_format()
 
 void fs_debug()
 {
-	
 	union fs_block block;
-	union fs_block indirect_block;
 
-	// Read in super block
 	disk_read(0,block.data);
-	int valid_super_block = check_magic(block.super.magic);
 
 	printf("superblock:\n");
-	if(valid_super_block)
-		printf("    magic number is valid\n");
-	else{
-		printf("    magic number is not valid\n");
-		return;
-	}
 	printf("    %d blocks\n",block.super.nblocks);
 	printf("    %d inode blocks\n",block.super.ninodeblocks);
 	printf("    %d inodes\n",block.super.ninodes);
-
-	// Go through inode blocks
-	for(int i=1; i<=block.super.ninodeblocks; i++){ //added equal
-		// Read in inode block
-		disk_read(i, block.data);
-
-		// Traverse inodes
-		for(int j = 0; j<INODES_PER_BLOCK; j++) {
-			// Check if inode is valid
-			if(block.inode[j].isvalid) {
-				int inumber = get_inum(i, j);
-				printf("inode %d:\n", inumber);
-				printf("    size: %d bytes\n", block.inode[j].size);
-
-				// Traverse direct pointers
-				if(block.inode[j].size > 0){
-					printf("    direct blocks: ");
-					print_valid_blocks(block.inode[j].direct, POINTERS_PER_INODE);
-				}
-
-				// Traverse indirect pointers
-				if(block.inode[j].indirect != 0){
-					printf("    indirect block: %d\n", block.inode[j].indirect);
-					printf("    indirect data blocks: ");
-					disk_read(block.inode[j].indirect, indirect_block.data);
-					print_valid_blocks(indirect_block.pointers, POINTERS_PER_BLOCK);
-				}
-			}
-		}
-
-	}
 }
 
 int fs_mount()
